a85a32729c613d9d77abac55fa76af5003afceaa
Gabriel Cacuango <gabrielcacuango07@gmail.com>
2020-05-11 16:37:15 -0500

    Implementacion de  comprobacion mayor de la media

--- src/controllers/CountryController.js
+++ src/controllers/CountryController.js
@@ -28,37 +28,64 @@ const importCSV = async path => {
 }
 
 const getAverage = (data, year) => {
-    if (isNaN(year)) {
-        throw new Error('El año ingresado no es valido.')
-    }
-    let average = 0;
-    data.forEach(element => {
-        let value = parseFloat(element[`${year}`]);
-        console.log(value)
-        if (!isNaN(value)) {
-            average += value;
+        if (isNaN(year)) {
+            throw new Error('El año ingresado no es valido.')
         }
-    });
-    average = parseFloat((average / data.length).toFixed(2))
-    return average
+        let average = 0;
+        data.forEach(element => {
+            let value = parseFloat(element[`${year}`]);
+            //console.log(value)
+            if (!isNaN(value)) {
+                average += value;
+            }
+        });
+        average = parseFloat((average / data.length).toFixed(2))
+        return average
 
-    /* Validacion del codigo de pais para despues */
-    // let country = data.filter(country => country['Country Code'] === countryCode)
-    // if (country.length === 0) {
-    //     throw new Error(
-    //         'El código del pais no es valido, asegurese de usar la especificación ISO 3166 ALPHA-3.')
-    // }
+        /* Validacion del codigo de pais para despues */
+        // let country = data.filter(country => country['Country Code'] === countryCode)
+        // if (country.length === 0) {
+        //     throw new Error(
+        //         'El código del pais no es valido, asegurese de usar la especificación ISO 3166 ALPHA-3.')
+        // }
 
 
 
+
+    }
+    //Gabriel
+
+const isGraderThanAverage = (data, year, country, prom) => {
+    let answer = false;
+    data.forEach(element => {
+        let codigo = element['Country Code'];
+        if (codigo === country) {
+            //console.log('es pais existe');
+            let pais = element[`${year}`];
+            if (pais > prom) {
+                //console.log(pais);
+                //console.log('El valor del pais es mayor a la media ');
+                answer = true;
+                return
+            }
+            //console.log(pais);
+
+
+
+        }
+
+    })
+    return answer;
 
 }
 
 
 /* pruebas */
-const tests = async () => {
-    let data = await importCSV('/home/will/Descargas/doc.csv')
+const tests = async() => {
+    let data = await importCSV('C:\\Users\\Gabriel\\Desktop\\archivos\\doc.csv')
     let prom = getAverage(data, 2015)
+    console.log(prom);
+    console.log(isGraderThanAverage(data, 2015, 'ECU', prom));
 
 }
 
