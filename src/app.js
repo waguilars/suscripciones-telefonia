77a5abc2cda6dd4a5fab85af2df100c580234816
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-05-12 21:28:22 -0500

    server in modules

--- src/app.js
+++ src/app.js
@@ -1,33 +1,20 @@
 /* archivo principal */
 // TODO: Chino
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-
 const argv = require('./configs/yargs').argv;
 const tareas = require('./controllers/CountryController')
+const server = require('./server');
 
 let comand = argv._[0];
-let objeto = []
 
 
 switch (comand) {
     case 'publicar':
-        objeto.push({
-            file: argv.file,
-            country: argv.country,
-            year: argv.year
-        })
-        const server = http.createServer((req, res) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end(`${objeto[0].file}`)
-        });
-        server.listen(port, hostname, () => {
-            console.log(`Server running at http://${hostname}:${port}/`);
-            //server.close();
-        });
+        const data = {
+            media: 'media',
+            top: [1, 2, 3, 4, 5]
+        }
+        server.startServer(data)
         break;
     case 'guardar':
         console.log('guardar los resultados');
